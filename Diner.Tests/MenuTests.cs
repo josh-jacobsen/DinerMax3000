7b38783c06b411f93d5f81c14d772f8fc9a78c86
Josh Jacobsen <joshjacobsen5@gmail.com>
2017-01-28 19:49:54 +1300

    basic tests working

---
+++ Diner.Tests/MenuTests.cs
@@ -0,0 +1,44 @@
+﻿using DinerMax3000Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diner.Tests
+{
+    [TestClass]
+    public class MenuTests
+    {
+   
+        [TestMethod]
+        public void CreatesMenuItem()
+        {
+            MenuItem salmon = new MenuItem();
+            salmon.Title = "Salmon";
+            salmon.Description = "Fresh Norwegian Salmon";
+            salmon.Price = 25.50;
+
+            Assert.AreEqual(25.50, salmon.Price);
+            Assert.AreEqual("Salmon", salmon.Title);
+
+
+
+        }
+
+        [TestMethod]
+        public void AddsMenuItemToMenu()
+        {
+            MenuItem cupcake = new MenuItem();
+
+            Menu summerMenu = new Menu();
+
+            summerMenu.items.Add(cupcake);
+
+            Assert.IsTrue(summerMenu.items.Contains(cupcake));
+
+        }
+
+    }
+}
--- DinerMax3000Console/Program.cs
+++ DinerMax3000Console/Program.cs
@@ -10,15 +10,7 @@ namespace DinerMax3000Console
     {
         static void Main(string[] args)
         {
-            Menu summerMenu = new Menu();
-            summerMenu.Name = "Summer menu";
 
-            MenuItem salmon = new MenuItem();
-            salmon.Title = "Salmon";
-            salmon.Description = "Fresh Norwegian Salmon";
-            salmon.Price = 25.50;
-
-            summerMenu.items.Add(salmon);
         }
     }
 }
